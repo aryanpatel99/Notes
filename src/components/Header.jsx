e2505a2b5c3e1ab754d80f1fc76adf831bde2746
Aryan Patel <aryanpatel6215@gmail.com>
2025-04-22 14:16:53 +0530

    added padding,margin

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 const Header = ({handleToggleDarkMode}) => {
   return (
     <div className='header'>
+      <div className='header-content'>
         <h1>Notes</h1>
         <button onClick={()=>handleToggleDarkMode((prevDarkMode)=>!prevDarkMode)} className='header-btn'>Toggle Mode</button>
+        </div>
     </div>
   )
 }
